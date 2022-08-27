d83d1963f4c390f3a6775bb30af2d509f1b101ee
Elnur Ruzmanov <80697079+elnurruzmanov@users.noreply.github.com>
2022-08-27 03:26:47 +0300

    I added the activeClassName class

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,27 +1,64 @@
-import React from 'react'
-import "./Sidebar.scss"
-import {Link, NavLink} from "react-router-dom"
-import Home from '../Lib/Home'
-import Logo from '../Lib/Svgs/Logo'
+import React from "react";
+import "./Sidebar.scss";
+import { Link, NavLink } from "react-router-dom";
+import Home from "../Lib/Home";
+import Logo from "../Lib/Svgs/Logo";
 
 const Sidebar = () => {
   return (
-    <div className='sidebar'>
-      <Link to="/" className='logo'>
+    <div className="sidebar">
+      <Link to="/" className="logo">
         <Logo />
       </Link>
-      <ul className='sidebar_list'>
-        <li className='sidebar_item '>
-          <NavLink to="/" className="sidebar_link">
-            <Home />
+      <ul className="sidebar_list">
+        <li className="sidebar_item ">
+          <NavLink to="/" className="sidebar_link sidebar_link-active" activeClassName="sidebar_link-active" exact>
+            <span className="sidebar_link-item sidebar_link-active-item">
+              <Home />
+            </span>
+          </NavLink>
+        </li>
+        <li className="sidebar_item ">
+          <NavLink to="/discount" className="sidebar_link" activeClassName="sidebar_link-active">
+            <span className="sidebar_link-item">
+              <Home />
+            </span>
+          </NavLink>
+        </li>
+        <li className="sidebar_item ">
+          <NavLink to="/graph" className="sidebar_link" activeClassName="sidebar_link-active">
+            <span className="sidebar_link-item">
+              <Home />
+            </span>
+          </NavLink>
+        </li>
+        <li className="sidebar_item ">
+          <NavLink to="/order" className="sidebar_link" activeClassName="sidebar_link-active">
+            <span className="sidebar_link-item">
+              <Home />
+            </span>
+          </NavLink>
+        </li>
+        <li className="sidebar_item ">
+          <NavLink to="/notification" className="sidebar_link" activeClassName="sidebar_link-active">
+            <span className="sidebar_link-item">
+              <Home />
+            </span>
+          </NavLink>
+        </li>
+        <li className="sidebar_item ">
+          <NavLink to="/settings" className="sidebar_link" activeClassName="sidebar_link-active">
+            <span className="sidebar_link-item">
+              <Home /> 
+            </span>
           </NavLink>
         </li>
       </ul>
-           <button className='sidebar_btn'>
-            <Home />
-           </button>
+      <button className="sidebar_btn">
+        <Home />
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
